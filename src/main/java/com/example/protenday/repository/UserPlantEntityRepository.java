e93d1392c1e2a8f48e12a48388e3c649f455c641
bang-star <bangseongmin@naver.com>
2023-03-27 23:58:25 +0900

    #5 - 도메인 업데이트
    1. 숲 생성
    2. 식물 타입 삭제
    3. 메시지 생성

--- src/main/java/com/example/protenday/repository/UserPlantEntityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.protenday.repository;
-
-import com.example.protenday.domain.UserPlantEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserPlantEntityRepository extends JpaRepository<UserPlantEntity, Long> {
-}
