f4a78bec6941756779ad54f4b4ba685222fff102
bang-star <bangseongmin@naver.com>
2023-04-01 19:50:54 +0900

    #15 - 식물 서비스 로직 구현
    CRUD 기능을 구현 완료.

---
+++ src/main/java/com/example/protenday/dto/Plant.java
@@ -0,0 +1,37 @@
+package com.example.protenday.dto;
+
+import com.example.potenday.domain.PlantEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Plant {
+    private Long id;
+    private String name;
+    private String attribute;
+    private String description;
+    private LocalDateTime registeredAt;
+    private String registeredBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
+
+    public static Plant fromEntity(PlantEntity entity) {
+        return Plant.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .attribute(entity.getAttribute())
+                .description(entity.getDescription())
+                .registeredAt(entity.registeredAt)
+                .registeredBy(entity.registeredBy)
+                .modifiedAt(entity.modifiedAt)
+                .modifiedBy(entity.modifiedBy)
+                .build();
+    }
+}
